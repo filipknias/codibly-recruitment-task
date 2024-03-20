79356c954553bd97c36d402587173ce6e08e09bc
Filip Kniaś <filipknias@MacBook-Air-Filip.local>
2024-03-20 15:00:44 +0100

    product modal component

---
+++ src/components/app/ProductModal.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from "react";
+import { TProduct } from "../../types/api";
+
+type TProps = {
+    product: TProduct;
+    onClose: () => void;
+}
+
+export default function ProductModal({ product, onClose }: TProps) {
+    const modalRef = useRef<HTMLDivElement|null>(null);
+
+    useEffect(() => {
+        const clickOutsideFn = (e: Event) => {
+            const targetEl = e.target as HTMLElement;
+            if (modalRef.current && !modalRef.current.contains(targetEl)) onClose();
+        };
+        
+        document.addEventListener("mousedown", (e) => clickOutsideFn(e));
+        return () => document.removeEventListener("mousedown", clickOutsideFn);
+    }, [modalRef]);
+
+    return (
+        <div className="h-full w-full bg-black bg-opacity-80 fixed inset-0 z-50">
+            <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full" ref={modalRef}>
+                <div className="bg-white rounded-lg p-5 max-w-2xl mx-auto">
+                    <div className="flex items-center justify-between gap-4 flex-wrap mb-8">
+                        <h1 className="text-2xl font-semibold">Product Details</h1>
+                        <button 
+                            className="rounded-lg bg-blue-500 text-white hover:bg-blue-600 transition duration-200 px-4 py-2" 
+                            onClick={onClose}
+                        >
+                            Close
+                        </button>
+                    </div>
+                    {Object.entries(product).map(([key, value]) => (
+                        <div key={key} className="mb-2">
+                            <span className="text-lg">{key}: </span>
+                            <span className="text-lg font-semibold">{value}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/app/ProductsTable.tsx
+++ src/components/app/ProductsTable.tsx
@@ -1,46 +1,58 @@
+import { useState } from "react";
 import { TProduct } from "../../types/api"
+import ProductModal from "./ProductModal";
 
 type TProps = {
     products: TProduct[];
 }
 
 export default function ProductsTable({ products }: TProps) {
+    const [openProduct, setOpenProduct] = useState<TProduct|null>(null);
+
     return (
-        <div className="bg-white rounded-lg mb-8">
-            <div className="overflow-x-auto">
-                <div className="inline-block min-w-full overflow-hidden">
-                    <table className="table-auto w-full">
-                        <thead>
-                            <tr>
-                                <th className="pl-9 h-20 uppercase font-semibold whitespace-nowrap border-b border-gray-200 text-gray-500">Product ID</th>
-                                <th className="p-5 h-20 uppercase font-semibold whitespace-nowrap border-b border-gray-200 text-gray-500">Name</th>
-                                <th className="p-5 h-20 uppercase font-semibold whitespace-nowrap border-b border-gray-200 text-gray-500">Year</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {products.map(({ id, name, year, color }) => (
-                                <tr key={id} style={{ backgroundColor: color }} className="cursor-pointer transition duration-200">
-                                    <td>
-                                        <div className="flex items-center justify-center p-5 h-20 text-center">
-                                            <span className="text-lg font-heading font-medium whitespace-nowrap">{id}</span>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div className="flex items-center justify-center p-5 h-20 text-center">
-                                            <span className="text-lg font-heading font-medium whitespace-nowrap">{name}</span>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <div className="flex items-center justify-center p-5 h-20 text-center">
-                                            <span className="text-lg font-heading font-medium whitespace-nowrap">{year}</span>
-                                        </div>
-                                    </td>
+        <>
+            {openProduct && <ProductModal product={openProduct} onClose={() => setOpenProduct(null)} />}
+            <div className="bg-white rounded-lg mb-8">
+                <div className="overflow-x-auto">
+                    <div className="inline-block min-w-full overflow-hidden">
+                        <table className="table-auto w-full">
+                            <thead>
+                                <tr>
+                                    <th className="pl-9 h-20 uppercase font-semibold whitespace-nowrap border-b border-gray-200 text-gray-500">Product ID</th>
+                                    <th className="p-5 h-20 uppercase font-semibold whitespace-nowrap border-b border-gray-200 text-gray-500">Name</th>
+                                    <th className="p-5 h-20 uppercase font-semibold whitespace-nowrap border-b border-gray-200 text-gray-500">Year</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-        </div>
-        </div>
+                            </thead>
+                            <tbody>
+                                {products.map((product) => (
+                                    <tr 
+                                        key={product.id} 
+                                        style={{ backgroundColor: product.color }} 
+                                        className="cursor-pointer transition duration-200"
+                                        onClick={() => setOpenProduct(product)}
+                                    >
+                                        <td>
+                                            <div className="flex items-center justify-center p-5 h-20 text-center">
+                                                <span className="text-lg font-heading font-medium whitespace-nowrap">{product.id}</span>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div className="flex items-center justify-center p-5 h-20 text-center">
+                                                <span className="text-lg font-heading font-medium whitespace-nowrap">{product.name}</span>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div className="flex items-center justify-center p-5 h-20 text-center">
+                                                <span className="text-lg font-heading font-medium whitespace-nowrap">{product.year}</span>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+            </div>
+            </div>
+        </>
     )
 }
